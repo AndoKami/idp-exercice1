298ec82fbed8f5a0f815c89a65e0a1d9c3eb2177
AndoKami <hei.ando.1007@gmail.com>
2025-01-13 14:54:28 +0300

    add: adding firebase cred

--- src/firebase.js
+++ src/firebase.js
@@ -2,9 +2,9 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
+    apiKey: "AIzaSyDgObKNfnEeBVZZQZLYAmPQ3GjXJHYYAJc",
+    authDomain: "idp-exrcise.firebaseapp.com",
+    projectId: "idp-exrcise",
 };
 
 const app = initializeApp(firebaseConfig);
